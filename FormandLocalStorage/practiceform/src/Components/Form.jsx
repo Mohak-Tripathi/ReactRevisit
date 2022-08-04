d777f9cad8ece7dd25d4d61454afb711570b4901
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-08-04 13:41:45 +0530

    localStorage done

---
+++ FormandLocalStorage/practiceform/src/Components/Form.jsx
@@ -0,0 +1,140 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import View from "./View"
+const getDataForm = () => {
+  const data = localStorage.getItem("books");
+
+  if (data) {
+    return JSON.parse(data);
+  } else {
+    return [];
+  }
+};
+
+
+const Form = () => {
+  const [books, setBooks] = useState(getDataForm());
+
+  const [formData, setFormData] = useState({
+    title: "",
+    author: "",
+    isbn: "",
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let book = {
+      title: formData.title,
+      author: formData.author,
+      isbn: formData.isbn,
+    };
+    setBooks([...books, book]);
+
+    setFormData({
+      title: "",
+      author: "",
+      isbn: "",
+    });
+  }
+
+  function deleteBook(id){
+   const filteredData=  books.filter((elem)=> elem.isbn !== id)
+   setBooks(filteredData)
+}
+
+  useEffect(() => {
+    console.log("books", books);
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    console.log(name, value);
+    setFormData({ ...formData, [name]: value });
+  }
+
+  return (
+    <div className='wrapper'>
+      <h1>Book list</h1>
+      <p> LocalStorage Practice Session</p>
+
+      <div className='main'>
+        <div className='form-container'>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label> Title</label>
+              <input
+                type='text'
+                name='title'
+                value={formData.title}
+                placeholder='Title'
+                required
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <label> Author </label>
+              <input
+                type='text'
+                name='author'
+                value={formData.author}
+                placeholder='Author'
+                required
+                onChange={handleChange}
+              />
+            </div>
+
+            <div>
+              <label> ISBN </label>
+              <input
+                type='text'
+                name='isbn'
+                value={formData.isbn}
+                placeholder='ISBN'
+                required
+                onChange={handleChange}
+              />
+            </div>
+            <button type='submit'> Submit</button>
+          </form>
+        </div>
+
+        <div className='view-container'>
+          {books.length > 0 && (
+            <>
+              <div className='table-responsive'>
+                <table className='table'>
+                  <thead>
+                    <tr>
+                      <th>ISBN#</th>
+                      <th>Title</th>
+                      <th>Author</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <View books={books} deleteBook={deleteBook} />
+                  </tbody>
+                </table>
+              </div>
+
+              <button
+                className='btn btn-danger btn-md'
+                onClick={() => setBooks([])}
+              >
+                Remove All
+              </button>
+            </>
+          )}
+          {books.length < 1 && <div>No books are added yet</div>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
---
+++ FormandLocalStorage/practiceform/src/Components/View.jsx
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+
+const View = ({ books, deleteBook }) => {
+
+  return books.map((book) => (
+    <tr key={book.isbn}>
+      <td> {book.isbn} </td>
+      <td> {book.title} </td>
+      <td> {book.author} </td>
+      <td className='delete-btn' onClick={() => deleteBook(book.isbn)}>
+        {" "}
+      Delete
+      </td>
+    </tr>
+  ));
+};
+
+export default View;
